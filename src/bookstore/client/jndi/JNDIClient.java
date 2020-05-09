4a65793abe69b8b41e7d35787d8374173ab23a07
hhugohm <hhugohm@gmail.com>
2020-05-09 11:48:15 -0500

    run JNDIClient

--- src/bookstore/client/jndi/JNDIClient.java
+++ src/bookstore/client/jndi/JNDIClient.java
@@ -1,7 +1,9 @@
 
 package bookstore.client.jndi;
 
+import bookstore.backend.annotations.SuppressLogging;
 import bookstore.backend.api.BookstoreDAO;
+import bookstore.backend.datamodel.Book;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
@@ -14,9 +16,11 @@ public class JNDIClient {
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) {
        
-        getBooksstoreDAO();
+        System.out.println(getBookById(1).toString());
         
     }
     
@@ -26,13 +30,23 @@ public class JNDIClient {
         try{
             Context ctx = new InitialContext();
             bookstoreDAO= (BookstoreDAO)ctx.lookup("java:global/BookstoreBackend/BookstoreDAO");
-            System.out.println("###### bokkstoreDAO: "+bookstoreDAO);
+           // System.out.println("###### bokkstoreDAO: "+bookstoreDAO);
+           
         
         }catch(Exception e){
             e.printStackTrace();
         }
         
-        return null;
+        return bookstoreDAO;
     } 
+    
+    
+    public static Book getBookById(int id) {
+        BookstoreDAO bookstoreDAO =getBooksstoreDAO();
+        Book book = bookstoreDAO.getBookById(id);
+        
+     return book;
+        
+    }
     
 }
