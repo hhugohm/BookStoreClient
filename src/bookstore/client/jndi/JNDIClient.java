be904f899a58b48eee24277b303a808b07166e50
hhugohm <hhugohm@gmail.com>
2020-05-23 08:18:09 -0500

    update code

--- src/bookstore/client/jndi/JNDIClient.java
+++ src/bookstore/client/jndi/JNDIClient.java
@@ -6,7 +6,11 @@ import bookstore.backend.api.BookstoreDAO;
 import bookstore.backend.api.ShoppingCartService;
 import bookstore.backend.dao.BookstoreDAOImpl;
 import bookstore.backend.datamodel.Book;
+import bookstore.backend.datamodel.enums.Availability;
+import bookstore.backend.datamodel.enums.BookFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
@@ -23,12 +27,18 @@ public class JNDIClient {
     
     public static void main(String[] args) {
         
-        System.out.println(getBookById(1).toString());
-        System.out.println("-----------------------------------------");
-        update(1);
-        System.out.println(getBookById(1).toString());
+        System.out.println(getBookById(12).toString());
+       // System.out.println("-----------------------------------------");
+       //update(12);
+        //System.out.println(getBookById(12).toString());
         
         //add();
+        /*Book book=buildBook();
+        System.out.println(book);
+        book.toString();
+        insert(book);*/
+        
+        //delete(11);
         
     }
     
@@ -47,7 +57,26 @@ public class JNDIClient {
         
         return bookstoreDAO;
     } 
-    
+     private static void insert(Book book){
+         BookstoreDAO bookstoreDAO = getBooksstoreDAO();
+        // Book book = buildBook();
+         bookstoreDAO.insert(book);
+    }
+      private static Book buildBook(){
+        Book book = new Book();
+         book.setTitle("My Book 1");
+         book.setDescription("This is my Book 1");
+         book.setAuthor("Author 1");
+         book.setPublisher("Publisher 1");
+         book.setPublishedDate(new Date());
+         book.setLanguage("English");
+         book.setNumberOfPages(111);
+         book.setFormat(BookFormat.PAPERBACK);
+         book.setPrice(11);
+         book.setAvailability(Availability.IN_STOCK);
+         book.setStockQuantity(10);
+        return book;
+    }
     
     public static Book getBookById(int id) {
         BookstoreDAO bookstoreDAO =getBooksstoreDAO();
@@ -57,11 +86,47 @@ public class JNDIClient {
         
     }
     
-    private static void update(int idBook){
+    private static void delete(int idBook){
          BookstoreDAO bookstoreDAO = getBooksstoreDAO();
          Book book= bookstoreDAO.getBookById(idBook);
-         book.setTitle("My Book 1 U2");
-         bookstoreDAO.update(book);
+         
+         //boolean bandera= (Optional.ofNullable(book).isPresent())?true:false;
+         
+         Optional<Book> optionalBook=Optional.ofNullable(book);
+         optionalBook.ifPresent(bookLamda ->{
+             bookstoreDAO.delete(bookLamda);
+         });
+         //optionalBook.ifPresent(bookstoreDAO::delete); 
+        
+        /* if( Optional.ofNullable(book).isPresent()){
+             bookstoreDAO.delete(book);
+         }else{
+               System.out.println("NO EXISTE BOOK ID= " +idBook);
+         }*/
+        
+        
+    }
+    
+    private static void update(int idBook){
+         
+         BookstoreDAO bookstoreDAO = getBooksstoreDAO();
+         Book book= bookstoreDAO.getBookById(idBook);
+         /*try{
+                Thread.currentThread().sleep(25000);
+            }catch(Exception e){
+                e.printStackTrace();
+            }*/
+         book.setTitle("My Book 1 U3");
+         try{
+              bookstoreDAO.update(book);
+         }catch(Exception e){
+              System.out.println("ERROR: ");
+             System.out.println( e.getCause());
+         }
+        
+         
+         System.out.println("### update exceuted....");
+          
     }
     
     
